94c754111b317ad1fb6fe83c88d0d72d367d72aa
Hari Kammana <hari.kammana@gmail.com>
2023-02-27 16:27:31 +0530

    initial spring boot web app

---
+++ src/main/java/in/javahome/smweb/SmwebApplication.java
@@ -0,0 +1,13 @@
+package in.javahome.smweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/javahome/smweb/SmwebApplicationTests.java
@@ -0,0 +1,13 @@
+package in.javahome.smweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
